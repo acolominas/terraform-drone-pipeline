76e4ddfd116983052e190360812f07295d39a46d
Arnau Colominas <acolominasilla@gmail.com>
2022-03-28 20:09:47 +0200

    test

---
+++ staging/data.tf
@@ -0,0 +1,23 @@
+
+#################################################################
+# NOTE: DO NOT MAKE CHANGES TO THIS FILE AS IT MAY BE OVERRIDEN #
+#################################################################
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-004914726163"
+    key            = "arnau/terraform.tfstate"
+    region         = "eu-west-1"
+    encrypt        = true
+    dynamodb_table = "tfstate-locking"
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
